2e885325bf51907289e4589d2bdca55c542fe4d3
Galacsh <galacsh.dev@gmail.com>
2022-01-09 18:34:55 +0900

    Updates

---
+++ component---src-pages-404-js-3c95d8b9f006a78e8a2c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_spaceful=self.webpackChunkgatsby_starter_spaceful||[]).push([[883],{9616:function(e,t,n){n.r(t);var a=n(7294),u=n(6634);t.default=function(){return a.createElement(a.Fragment,null,a.createElement(u.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3c95d8b9f006a78e8a2c.js.map
